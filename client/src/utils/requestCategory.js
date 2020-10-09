cf7fadf64d0ec5521bc6b3a46b15270227628bd9
derrick-aung <derrick.aungkhant@gmail.com>
2020-10-09 20:43:24 +0630

    requestCategories for different request endpoints

---
+++ client/src/utils/requestCategory.js
@@ -0,0 +1,8 @@
+// This file defines category types for sending requests to different endpoints in actions
+
+export default {
+  TRENDING: 'trending',
+  UPCOMING: 'upcoming',
+  GENRE: 'genre',
+  SEARCH: 'search',
+};
