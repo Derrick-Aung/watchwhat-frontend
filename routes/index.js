f5218a400e931f93c999d723b6409a04d2455610
derrick-aung <derrick.aungkhant@gmail.com>
2020-10-09 19:06:49 +0630

    Added dependencies and cleanup

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,3 @@ apiRouter.use('/auth', authRouter);
 apiRouter.use('/user', userRouter);
 
 module.exports = apiRouter;
-
-/*
-http://localhost:8080/api/auth/facebook
-http://localhost:8080/api/poll/expiry
-http://localhost:8080/api/poll/feed
-http://localhost:8080/api/poll/
-*/
