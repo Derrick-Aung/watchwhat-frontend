af50d7292d8def54a5902f4e17c2c80a3e6eae31
derrick-aung <derrick.aungkhant@gmail.com>
2020-10-15 21:27:08 +0630

    MovieDetail page

--- client/src/pages/MovieDetailPage.jsx
+++ client/src/pages/MovieDetailPage.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchMovie } from '../redux/movie/movieActions';
+import constants from '../components/constants';
 import styled from 'styled-components';
+import { TMDB_IMG_URL_OG } from '../config';
+import HorizontalWrappper from '../components/MovieDetail/HorizontalWrapper/HorizontalWrappper';
+import Detail from '../components/MovieDetail/Detail/Detail';
 
 const PageContent = styled.div`
   width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-bottom: 2rem;
 `;
 
 const MovieDetailPage = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const { movieId } = useParams();
+
+  const movie = useSelector((state) => state.movie.data);
+
+  useEffect(() => {
+    dispatch(fetchMovie(movieId));
+    return () => {};
+  }, []);
+
+  return (
+    <HorizontalWrappper backdrop={`${TMDB_IMG_URL_OG}${movie.backdrop_path}`}>
+      <Detail {...movie} />
+    </HorizontalWrappper>
+  );
 };
 
 export default MovieDetailPage;
