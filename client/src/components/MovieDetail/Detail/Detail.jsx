7fe4379bc65fd6b60e3edadf313bb39bcf364249
derrick-aung <derrick.aungkhant@gmail.com>
2020-10-15 21:26:57 +0630

    Detail component for movie data

---
+++ client/src/components/MovieDetail/Detail/Detail.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import constants from '../../constants';
+import { Typography } from '@material-ui/core';
+import RatingRuntime from '../../RatingRuntime/RatingRuntime';
+
+const DetailDiv = styled.div`
+  padding: 30px;
+`;
+
+const Title = styled.div`
+  font-family: ${constants.primaryFont} !important;
+  font-weight: 600;
+`;
+
+const FlexCenterDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Genres = ({ genres = [], style }) => {
+  const content = genres.map((g) => g.name).join(', ');
+  return (
+    <Typography variant="h5" style={style}>
+      {content}
+    </Typography>
+  );
+};
+
+const MetaData = ({ title, content, style }) => (
+  <div style={style}>
+    <Typography variant="h6" style={{ color: constants.primaryTextColorMuted }}>
+      {title}
+    </Typography>
+    <Typography variant="h6">{content}</Typography>
+  </div>
+);
+
+const Detail = ({
+  title,
+  vote_average,
+  runtime,
+  genres,
+  release_date,
+  budget,
+  overview,
+}) => {
+  return (
+    <DetailDiv>
+      <Typography variant="h3" component="h1">
+        <Title>{title}</Title>
+        <RatingRuntime
+          style={{ marginTop: '1rem' }}
+          runtime={runtime}
+          vote_average={vote_average}
+        />
+        <div>
+          <Genres genres={genres} style={{ marginTop: '1rem' }} />
+        </div>
+        <MetaData
+          title="Release Date"
+          content={release_date}
+          style={{ marginTop: '1rem' }}
+        />
+        <MetaData
+          title="Budget"
+          content={budget}
+          style={{ marginTop: '0.2rem' }}
+        />
+        <MetaData
+          title="Overview"
+          content={overview}
+          style={{ marginTop: '1rem' }}
+        />
+      </Typography>
+    </DetailDiv>
+  );
+};
+
+export default Detail;
