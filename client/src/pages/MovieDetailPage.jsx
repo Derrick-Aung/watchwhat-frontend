938ea3b323ab3bf9b2e5d2d883ffc883bad2c54a
derrick-aung <derrick.aungkhant@gmail.com>
2020-10-18 16:39:59 +0630

    Vote component and fetching vote

--- client/src/pages/MovieDetailPage.jsx
+++ client/src/pages/MovieDetailPage.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchMovie } from '../redux/movie/movieActions';
+import { fetchVote, clearVote } from '../redux/vote/voteActions';
 import constants from '../components/constants';
 import styled from 'styled-components';
 import { TMDB_IMG_URL_OG } from '../config';
 import HorizontalWrappper from '../components/MovieDetail/HorizontalWrapper/HorizontalWrappper';
 import Detail from '../components/MovieDetail/Detail/Detail';
+import Vote from '../components/Vote/Vote';
 
 const PageContent = styled.div`
   width: 60%;
@@ -23,12 +25,23 @@ const MovieDetailPage = () => {
 
   useEffect(() => {
     dispatch(fetchMovie(movieId));
-    return () => {};
+    dispatch(fetchVote(movieId));
+    return () => {
+      dispatch(clearVote());
+    };
   }, []);
 
   return (
     <HorizontalWrappper backdrop={`${TMDB_IMG_URL_OG}${movie.backdrop_path}`}>
       <Detail {...movie} />
+      <Vote
+        movieId={movieId}
+        style={{
+          position: 'fixed',
+          bottom: '16px',
+          right: '16px',
+        }}
+      />
     </HorizontalWrappper>
   );
 };
